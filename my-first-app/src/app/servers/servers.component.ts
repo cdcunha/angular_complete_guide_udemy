ddeecc794c5b473645e2ea98692513d6033cdd31
Charlestown <cdcunha@gmail.com>
2022-12-02 16:08:46 -0800

    Assesment about Data biding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,11 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-servers', //To use as component <app-servers></app-servers>
   //selector: '[app-servers]', //To use as an html attribute: <div app-servers></div>
   //selector: '.app-servers', //To use an html class: <div class="app-servers"></div>
-  //templateUrl: './servers.component.html', //Instead using templateUrl, we can use template inline
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html', //Instead using templateUrl, we can use template inline
+  //template: `<app-server></app-server>
+  //<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  userName = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);    
+  };
+
+  ngOnInit() {
+    
+  };
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event : Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
