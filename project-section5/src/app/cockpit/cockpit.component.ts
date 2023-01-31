b43a82cf76ae0603f95e8cb96fd1944cd226f9a5
Charlestown <cdcunha@gmail.com>
2023-01-30 19:48:53 -0800

    Using Binding property, biding event, Local Reference and ViewChild

--- project-section5/src/app/cockpit/cockpit.component.ts
+++ project-section5/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,23 +6,27 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent {
-  //Biding Custom events
+  //Biding Custom events. Ex: @Output() blueprintCreated 
+  //           OR with alias: @Output('bpCreated') blueprintCreated
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  //newServerName = ''; <-Commented because I'm using Local reference in template
+  //newServerContent = ''; <-Commented to use Local Reference by ViewChild
+  @ViewChild('serverContentInput', { static: true }) serverContentInput : ElementRef; //Using Local Reference in template by ViewChild
   
-  onAddServer(){
+  onAddServer(nameInput: HTMLInputElement){ //Using Local references in template passed by parameter
+    console.log('Server Name: ' + nameInput.value);
+    console.log('Server Content: ' + this.serverContentInput.nativeElement.value);
     this.serverCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint(){
+  onAddBlueprint(nameInput: HTMLInputElement){ //Using Local references in template passed by parameter
     this.blueprintCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
--- project-section5/src/app/server-element/server-element.component.ts
+++ project-section5/src/app/server-element/server-element.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //Changing the View Encapsulation: None, Emulated and ShadowDom
 })
 export class ServerElementComponent {
-  //Creating alias for custom property
+  //Creating custom property. Ex: @Input() element 
+  //              OR using alias: @Input('srvElement') element
   @Input('srvElement') element: {type: string; name: string, content: string};
 }
