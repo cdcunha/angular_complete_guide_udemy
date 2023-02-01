4594fb13cf9e5753e006ba263290846c959ddd19
Charlestown <cdcunha@gmail.com>
2023-02-01 12:31:39 -0800

    Passing Data with Event and Property Biding

--- project-section6/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-section6/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg') 
+    new Recipe('A Test Recipe', 'This is simply a test A', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is another simply a test B', 'https://upload.wikimedia.org/wikipedia/commons/9/93/Life_In_Recipes.png') 
   ]
+
+  onSelectedRecipe(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
