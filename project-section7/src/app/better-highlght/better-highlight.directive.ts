1fde1b5f3e2b11139fce61f21630bd531bee3fd6
Charlestown <cdcunha@gmail.com>
2023-02-01 18:49:31 -0800

    Biding to Directive Properties

--- project-section7/src/app/better-highlght/better-highlight.directive.ts
+++ project-section7/src/app/better-highlght/better-highlight.directive.ts
@@ -1,26 +1,29 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; //the parameter must be the same used in the nativeElement
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string; //the parameter must be the same used in the nativeElement
 
   constructor(private elRef: ElementRef, private rederer: Renderer2) { }
 
   ngOnInit(): void {
-    
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener("mouseenter") mouseover(eventData: Event) {
     //this.rederer.setStyle(this.elRef.nativeElement, 'background-color', 'blue'); //This is better if I don't access to the DOM
     this.rederer.setStyle(this.elRef.nativeElement, 'color', 'white');
-    this.backgroundColor = 'blue'; //Using HostBiding instead Rederer2
+    this.backgroundColor = this.highlightColor; //Using HostBiding instead Rederer2
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
     //this.rederer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent'); //This is better if I don't access to the DOM
     this.rederer.setStyle(this.elRef.nativeElement, 'color', 'black');
-    this.backgroundColor = 'transparent'; //Using HostBiding instead Rederer2
+    this.backgroundColor = this.defaultColor; //Using HostBiding instead Rederer2
   }
 }
