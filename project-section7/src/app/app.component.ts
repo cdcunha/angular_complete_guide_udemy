88140240f01ab3402a9eef01c4703d39dda4a462
Charlestown <cdcunha@gmail.com>
2023-02-01 16:57:34 -0800

    Initial code

---
+++ project-section7/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project-section7';
+
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
+}
