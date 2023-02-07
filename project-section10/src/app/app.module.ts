cf8cda63a2f41f23cfe76a01b7e5a2c0e3cc073f
Charlestown <cdcunha@gmail.com>
2023-02-07 08:58:23 -0800

    Implementing the RecipeService

--- project-section10/src/app/app.module.ts
+++ project-section10/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- project-section10/src/app/recipes/recipe.service.ts
+++ project-section10/src/app/recipes/recipe.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test A', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is another simply a test B', 'https://upload.wikimedia.org/wikipedia/commons/9/93/Life_In_Recipes.png') 
+  ]
 
   constructor() { }
+
+  getRecipes() {
+    //Use slice to pass a copy of array instead of the reference. So, if the array is modified outside this class will not affect the array here
+    return this.recipes.slice();
+  }
 }
