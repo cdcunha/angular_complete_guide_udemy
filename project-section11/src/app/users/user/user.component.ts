f076e78e5fe65992608f85eba31dc94894e76aa4
Charlestown <cdcunha@gmail.com>
2023-02-07 23:25:59 -0800

    Section11 - 133. Fetching Route Parameters

--- project-section11/src/app/users/user/user.component.ts
+++ project-section11/src/app/users/user/user.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: {id: number, name: string};
+
+  //ActivatedRoute will give access to the id in the url
+  constructor (private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+  }
 }
