20fe867d949d400326672c1c1414a9f65148e6c0
Charlestown <cdcunha@gmail.com>
2023-02-09 09:10:51 -0800

    Section11 - 148. Using a Fake Auth Service

--- project-section11/src/app/home/home.component.ts
+++ project-section11/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,17 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
-  constructor (private router: Router){}
+  constructor (private router: Router, private authService: AuthService){}
 
   onLoadServers(id: number){
     this.router.navigate(['/servers', id, 'edit'], { queryParams: { allowEdit: '1' }, fragment: 'loading' });
+  }
+
+  onLogin(){
+    this.authService.login();
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
 }
