4d761d31c0828af9fa202d379db5603028bdb9ad
Charlestown <cdcunha@gmail.com>
2023-02-12 12:48:46 -0800

    Section13: 172. Getting Closer to the Core of Observables

--- project-section13/src/app/home/home.component.ts
+++ project-section13/src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  firstObsSubscription: Subscription;
 
+  ngOnInit(): void {
+    //Building our own observable that every 1s will fire a console log
+    this.firstObsSubscription = interval(1000).subscribe(
+      count => {
+        console.log(count);
+      });
+  }
+
+  ngOnDestroy(): void {
+    //We need to unsubscribe the custom Observable to avoid Memory Leaks.
+    //If the Observable is provided by Angular we don't need to unsubscribe
+    this.firstObsSubscription.unsubscribe();
+  }
 }
