6b89d6cf8aa77dc8033ed6da778c9468115acb4e
Charlestown <cdcunha@gmail.com>
2023-02-12 15:58:31 -0800

    Section13: 174. Errors & Completion

--- project-section13/src/app/home/home.component.ts
+++ project-section13/src/app/home/home.component.ts
@@ -21,15 +21,25 @@ export class HomeComponent implements OnInit, OnDestroy {
       observer => {
         let count = 0;
         setInterval(() => {
-          observer.next(count)
+          observer.next(count);
+          if (count === 2){
+            observer.complete();
+          }
+          if (count > 3){
+            observer.error(new Error('Count is greater than 3!'));
+          }
           count++;
         }, 1000);
     });
 
     this.firstObsSubscription =customIntervalObservable.subscribe(
-      data => {
-        console.log(data);
-    });
+      data => { console.log(data); },
+      error => { 
+        console.log(error);
+        alert(error.message);
+      },
+      () => { console.log('Completed!')}
+    );
   }
 
   ngOnDestroy(): void {
