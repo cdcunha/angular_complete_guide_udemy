60b5ad5b9a3a95989974189ef3c0e5f161bac188
Charlestown <cdcunha@gmail.com>
2023-02-12 19:33:42 -0800

    Section15: 187. TD: Submitting and Using the Form

--- project-section15/src/app/app.component.ts
+++ project-section15/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,11 @@ export class AppComponent {
   title = 'project-section15';
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  //We don't need HTMLFormElement because we're using ngForm
+  //onSubmit(form: HTMLFormElement){
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 }
