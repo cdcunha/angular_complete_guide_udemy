549397cd517269d6831ff797ed47c53a5456ca72
Charlestown <cdcunha@gmail.com>
2023-04-02 16:20:11 -0700

    Section203: Creating a Form in code

--- project-section15-reactive/src/app/app.component.ts
+++ project-section15-reactive/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project-section15-reactive';
   genders = ['male', 'female'];
+  // To handle forms using Template-Driven we use NgForm, instead FormGroup (used to hadle forms using Reative mode)
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'), //Default value = male
+    })
+  }
 }
